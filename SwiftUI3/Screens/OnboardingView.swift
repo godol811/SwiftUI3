e8ab0bfc841eb69d53eb4f426ad32cc3ce2f2048
Jongchan Ko <godol811@naver.com>
2022-01-03 17:05:15 +0900

    22.Jan.03 HomeView & OnBoardingView Updated

--- SwiftUI3/Screens/OnboardingView.swift
+++ SwiftUI3/Screens/OnboardingView.swift
@@ -13,18 +13,95 @@ struct OnboardingView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     
     var body: some View {
-        VStack (spacing:20){
-            Text("Onboarding")
-                .font(.largeTitle)
-            Button(action: {
-                isOnboardingViewActive = false
-                //Some action
+        ZStack {
+            Color("ColorBlue")
+                .ignoresSafeArea(.all, edges: .all)
+            VStack (spacing:20){
                 
                 
-            }){
-                Text("Start")
-            }
-        }// :VStack
+                Spacer()
+                // MARK: - HEADER
+                VStack(spacing: 0){
+                    Text("Share.")
+                        .font(.system(size: 60))
+                        .fontWeight(.heavy)
+                        .foregroundColor(.white)
+                    
+                    Text("""
+                    It's not how much we give but
+                    how much love we put into giving.
+                    """)
+                        .font(.title3)
+                        .fontWeight(.light)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 10)
+                } // HEADER
+                // MARK: - CENTER
+                ZStack{
+                    CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+                    
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                    
+                } // CENTER
+               
+                Spacer()
+                
+                // MARK: - FOOTER
+                ZStack{
+                    // PARTS OF THE CUSTOM BUTTON
+                  
+                    
+                    // 1. BACKGROUND (STATIC)
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    // 2. CALL-TO-ACTION (STATIC)
+                    Text("Get Started")
+                        .font(.system(.title3, design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x: 20)
+                    
+                    // 3. CAPSULE (DYNAMIC WIDTH)
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width: 80)
+                        Spacer()
+                    }
+                    
+                    // 4. CIRCLE (DRAGGABLE)
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24, weight: .bold))
+                            
+                        }
+                        .foregroundColor(.white)
+                        .frame(width: 80, height: 80, alignment: .center)
+                        .onTapGesture {
+                            isOnboardingViewActive = false
+                        }
+                        
+                        Spacer()
+                    } // : HStack
+                } // : FOOTER
+                .frame( height: 80, alignment: .center)
+                .padding()
+                
+            } // :VStack
+        } // :ZStack
        
     }
 }
